d4922870f10ca4f6f67b8159054bbc51984fb60b
antonio-gor <antonio.gor@hotmail.com>
2020-10-02 22:20:20 -0700

    complete flutter/app_template

---
+++ app_template/lib/home_widget.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'placeholder_widget.dart';
+
+class Home extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _HomeState();
+  }
+}
+
+class _HomeState extends State<Home> {
+  int _currentIndex = 0;
+  final List<Widget> _children = [
+    PlaceholderWidget(Colors.white60),
+    PlaceholderWidget(Colors.deepOrange),
+    PlaceholderWidget(Colors.lightGreen),
+    PlaceholderWidget(Colors.deepPurple)
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('My Flutter App'),
+      ),
+      body: _children[_currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: onTabTapped,
+        currentIndex: _currentIndex,
+        items: [
+          BottomNavigationBarItem(
+            icon: new Icon(Icons.home),
+            title: new Text('Home'),
+          ),
+          BottomNavigationBarItem(
+            icon: new Icon(Icons.music_note),
+            title: new Text('Music'),
+          ),
+          BottomNavigationBarItem(
+            icon: new Icon(Icons.mail),
+            title: new Text('Messages'),
+          ),
+          BottomNavigationBarItem(
+            icon: new Icon(Icons.person),
+            title: new Text('Profile'),
+          ),
+        ],
+      ),
+    );
+  }
+
+  // Reruns build method on new tab by index
+  void onTabTapped(int index) {
+    setState(() {
+      _currentIndex = index;
+    });
+  }
+}
